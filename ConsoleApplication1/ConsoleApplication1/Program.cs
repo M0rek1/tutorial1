72aa0c3849ee4fe2a94edf01206558fb1b660d51
s28214 <s28214@pjwstk.edu.pl>
2024-03-12 11:31:39 +0100

    change on new branch 1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleApplication1
 {
@@ -9,6 +10,24 @@ namespace ConsoleApplication1
             Console.WriteLine("fff");
             Console.WriteLine("fff2");
             Console.WriteLine("fff3");
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            double average = CalculateAverage(numbers);
+            Console.WriteLine("Average: " + average);
+
+
+
+        }
+
+        public static double CalculateAverage(int[] array)
+        {
+            int sum = 0;
+            foreach (int num in array)
+            {
+                sum += num;
+            }
+
+            return (double)sum / array.Length;
         }
     }
 }
+
